50eff16444282ef793fa300affa7781d577a9a77
Kit Gallagher <gallagherkit@outlook.com>
2021-11-25 12:46:00 +0000

    Gif generation (#6)

--- spatial_main.py
+++ spatial_main.py
@@ -10,24 +10,6 @@ kappa = 1
 A_0, B_0 = 1.0, 1.0  # Should be floats
 
 m = Grid1D(nx=100, Lx=1.)
-# v = CellVariable(mesh=m, hasOld=True, value=[[A_0], [B_0]], elementshape=(2,))
-
-
-# eqn = TransientTerm([[1, 0],
-#                      [0, 1]]) == DiffusionTerm([[[D_1, 0],
-#                                                  [0, D_2]]]) \
-
-# # Default constaints in fipy are zero flux BC
-
-# vi = Viewer((v[0], v[1]))
-
-# from builtins import range
-# for t in range(10):
-#     v.updateOld()
-#     eqn.solve(var=v, dt=1.e-3)
-#     vi.plot(f"Images/Spatial_ODE/newimage{t}.png")
-
-####
 
 v0 = CellVariable(mesh=m, hasOld=True, value=A_0)
 v1 = CellVariable(mesh=m, hasOld=True, value=B_0)
--- spatial_main_2d.py
+++ spatial_main_2d.py
@@ -1,13 +1,22 @@
-"""Runs simulation of time and spatially dependant ODE model"""
+"""Runs simulation of time and spatially dependant ODE model
 
+Plots one concentration variable at subsequent timepoints and then converts 
+these into a .gif, optionally deleting the image files afterwards"""
+
+
+import glob
+import os
 from fipy import Grid2D, CellVariable, TransientTerm, DiffusionTerm, Viewer
+from PIL import Image
+from numpy import delete
+
 
 D_A, D_B = 1.6, 160
 alpha = 0.02
 beta = 3
 mu = 1
 kappa = 1e-6
-A_0, B_0 = 200, 75
+A_0, B_0 = 200, 75  # Shou;d be defined as floats
 
 nx = 20
 ny = nx
@@ -18,15 +27,15 @@ m = Grid2D(dx=dx, dy=dy, nx=nx, ny=ny)
 
 time_step = 0.002
 step_num = 40
+delete_images = True  # Boolean to delete images after running
 
 v0 = CellVariable(name = "Concentration of A", mesh=m, hasOld=True, value=A_0)
 v1 = CellVariable(name = "Concentration of B", mesh=m, hasOld=True, value=B_0)
-plot_var = v1  # CHANGE THIS TO V0 OR V1 TO DETERMINE WHICH VARIABLE IS PLOTTED
+plot_var = v0  # CHANGE THIS TO V0 OR V1 TO DETERMINE WHICH VARIABLE IS PLOTTED
 
 eqn0 = TransientTerm(var=v0) == mu - alpha * v0 + kappa * v0**2 * v1 + DiffusionTerm(D_A, var=v0) 
 eqn1 = TransientTerm(var=v1) == beta - kappa * v0**2 * v1 +  DiffusionTerm(D_B, var=v1)
 eqn = eqn0 & eqn1
-
 
 viewer = Viewer(vars=plot_var)
 for step in range(step_num):
@@ -35,3 +44,18 @@ for step in range(step_num):
     eqn.solve(dt=time_step)
     viewer.plot(f"Images/Spatial_ODE/Mesh2D_{step}.png")
 
+
+fp_in = "Images/Spatial_ODE/Mesh2D_*.png"
+fp_out = "Images/Spatial_ODE/Mesh2D.gif"
+
+img, *imgs = [Image.open(f) for f in sorted(glob.glob(fp_in))]
+img.save(fp=fp_out, format='GIF', append_images=imgs,
+         save_all=True, duration=200, loop=0)
+
+if delete_images:
+    for file in os.listdir('Images/Spatial_ODE/'):
+        if file.endswith('.png'):
+            try:
+                os.remove('Images/Spatial_ODE/' + file) 
+            except FileNotFoundError:
+                print("Can't find file: " + str(file))
