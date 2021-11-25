2cd6f21afc4f1f93c1d81ce802153bd6d285f5c3
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-11-25 14:41:58 +0000

    minor changes

--- stochastic_workflow.py
+++ stochastic_workflow.py
@@ -2,13 +2,13 @@ import sim
 import numpy as np
 import matplotlib.pyplot as plt
 
-#### Set gridsize parameters ###
+# Set gridsize parameters ###
 
-h= 25*1e-3
+h = 25*1e-3  #mm
 
-m = 100  # number of rows
+m = 2  # number of rows
 n = 100 # number of columns
-domain_size = (h*m,h*n) # = 1mm 
+domain_size = (h*m,h*n) # = 1mm
 
 ### Set the rate parameters  ###
 
@@ -18,23 +18,19 @@ k2 = 64000
 k4 = 192000
 # Death rate for species A
 k3 = .02
-# Reaction rate for "2A + B -> 3A" 
+# Reaction rate for "2A + B -> 3A"
 k1 = 2.44*1e-16
-
-# Diffusion parameters for species A and B
-d_A = 1e-5/(2*h**2)
-d_B = 1e-3/(2*h**2)
 
 # Initialise
 A_init = 200
 B_init = 75
 
 # Store all the parameters in a dictionary
-params = {'mu': k2*h**3, 'beta': k4*h**3, 'alpha': k3, 'kappa': k1/h**6, 
-          'd_A': d_A, 'd_B': d_B}
+params = {'mu': k2*h**3, 'beta': k4*h**3, 'alpha': k3, 'kappa': k1/h**6,
+          'd_A': 1e-5/(2*h**2), 'd_B': 1e-3/(2*h**2)}
 
-tau = 0.002 # time interval
-N_t = 1_000_000  # number of units of time
+tau = .2 # time interval
+N_t = 1_00_000  # number of units of time
 
 X_A, X_B = sim.initialize_picture(m, n, A_init, B_init)
 
@@ -44,6 +40,8 @@ for t in np.arange(N_t-1):
         print(t)
     X_A, X_B = sim.calculate_picture(tau, X_A, X_B, **params)
 
+plt.plot(X_A[0, :])
+plt.show()
 plt.imshow(X_A)
 plt.show()
 plt.imshow(X_B)
