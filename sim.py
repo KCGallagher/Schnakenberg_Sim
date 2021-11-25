45c466758feaf2b1e33d892bfd7caa517cf9223d
Jesse Murray <jessebmurray1@gmail.com>
2021-11-25 10:56:39 +0000

    lower memory movie

--- sim.py
+++ sim.py
@@ -104,7 +104,7 @@ def diffuse(tau, M, d):
     return Z
 
 
-def calculate(tau, X_A, X_B, t, mu, beta, alpha, kappa, d_A, d_B):
+def calculate_movie(tau, X_A, X_B, t, mu, beta, alpha, kappa, d_A, d_B):
     """
     Calculate the number of molecules in each cell of the A and B grids
     for time t+1.
@@ -131,3 +131,26 @@ def calculate(tau, X_A, X_B, t, mu, beta, alpha, kappa, d_A, d_B):
     # Calculate next grid
     X_A[t+1] = X_A[t] + Z_A_birth + Z_A_death + Z_A_react + Z_A_diffusion
     X_B[t+1] = X_B[t] + Z_B_birth + Z_B_react + Z_B_diffusion
+
+
+def initialize_movie(N_t, m, n, A_init, B_init):
+    """
+    Initializes the 2D grid for a movie with N_t time points,
+    m rows, and n columns. And with initial A and B set to
+    A_init and B_init.
+    
+    Initializes the parameters with signed 16-bit integer, which
+    can go from -32_768 to 32_767.
+    """    
+    
+    # Initialize the grid of cells for A and B populations
+    shape = (N_t, m, n)  # index by time, row, column
+
+    X_A = np.zeros(shape, dtype=np.int16)  # set A population at all times to zero
+    X_A[0] += A_init  # add initial A population for time zero
+
+    X_B = np.zeros(shape, dtype=np.int16)  # do the same as above for B...
+    X_B[0] += B_init
+
+    return X_A, X_B
+
