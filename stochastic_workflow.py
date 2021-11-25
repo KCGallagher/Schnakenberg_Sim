4f2f9f29a645fe7e396fcba2861e180d12ed9f06
Elizabeth Hayman <elizabeth.hayman@keble.ox.ac.uk>
2021-11-25 16:08:21 +0000

    addImage

--- stochastic_workflow.py
+++ stochastic_workflow.py
@@ -6,8 +6,8 @@ import matplotlib.pyplot as plt
 
 h = 25*1e-3  #mm
 
-m = 2  # number of rows
-n = 100 # number of columns
+m = 1  # number of rows
+n = 40 # number of columns
 domain_size = (h*m,h*n) # = 1mm
 
 ### Set the rate parameters  ###
@@ -29,7 +29,7 @@ B_init = 75
 params = {'mu': k2*h**3, 'beta': k4*h**3, 'alpha': k3, 'kappa': k1/h**6,
           'd_A': 1e-5/(2*h**2), 'd_B': 1e-3/(2*h**2)}
 
-tau = .2 # time interval
+tau = .02 # time interval
 N_t = 1_00_000  # number of units of time
 
 X_A, X_B = sim.initialize_picture(m, n, A_init, B_init)
@@ -42,10 +42,14 @@ for t in np.arange(N_t-1):
 
 fig, ax1 = plt.subplots()
 ax1.plot(X_A[0, :], label = 'A')
+ax1.plot([],label = 'B', linestyle = 'dashed', color = 'r')
 ax2 = ax1.twinx()
+ax2.set_ylim(0,150)
 ax2.plot(X_B[0,:], label = 'B', linestyle = 'dashed', color = 'r')
+ax1.legend()
+fig.tight_layout()
 plt.show()
-#plt.imshow(X_A)
+plt.imshow(X_A)
+plt.show()
+#plt.imshow(X_B)
 #plt.show()
-plt.imshow(X_B)
-plt.show()
